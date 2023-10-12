3eb4675471f4fa9887d5febc4306e4a48102610d
SirCodiac <118847092+Secodity@users.noreply.github.com>
2023-10-12 17:56:05 +0200

    Implemented a way to provide multiple directories to delete

--- Wipedir/Wipedir/Program.cs
+++ Wipedir/Wipedir/Program.cs
@@ -11,9 +11,9 @@ public static class Program
 
         var startingDirectory = new Option<string>(name: "--start", description: "The starting directory") { IsRequired = true };
         startingDirectory.AddAlias("-s");
-        var directories = new Option<string[]>(name: "--dirs", description: "Directories to delete") { IsRequired = true };
+        var directories = new Option<string[]>(name: "--dir", description: "Directory to delete. For multiple directories provide the '-d' argument up to 10 times.") { IsRequired = true, Arity = new ArgumentArity(1, 10) };
         directories.AddAlias("-d");
-        var force = new Option<bool>(name: "--force", description: "Force deletion (currently not implemented", getDefaultValue: () => false);
+        var force = new Option<bool>(name: "--force", description: "Force deletion (currently not implemented)", getDefaultValue: () => false);
         force.AddAlias("-f");
         var recursive = new Option<bool>(name: "--recursive", description: "Recursive search", getDefaultValue: () => false);
         recursive.AddAlias("-r");
@@ -21,20 +21,23 @@ public static class Program
 
         var rootCommand = new RootCommand("Wipedir");
         rootCommand.AddOption(startingDirectory);
-        rootCommand.AddOption(directory);
+        rootCommand.AddOption(directories);
         rootCommand.AddOption(force);
         rootCommand.AddOption(recursive);
 
         rootCommand.SetHandler((startDirValue, dirValue, forceValue, recursiveValue) =>
         {
             __Execute(startDirValue, dirValue, forceValue, recursiveValue);
-        }, startingDirectory, directory, force, recursive);
+        }, startingDirectory, directories, force, recursive);
 
         await rootCommand.InvokeAsync(args);
     }
 
     private static void __Execute(string startDir, string[] directories, bool force, bool recursive)
     {
+#if DEBUG
+        __PrintArgs(startDir, directories, force, recursive);
+#endif
         __ValidateStartDirectory(startDir);
 
         var FolderPathes = __GetMatchingFolderPathes(startDir, directories, recursive);
@@ -66,15 +69,15 @@ public static class Program
         }
     }
 
-    private static void __PrintArgs(string startDir, string directory, bool force, bool recursive)
+    private static void __PrintArgs(string startDir, string[] directories, bool force, bool recursive)
     {
         Console.WriteLine($"StartDir: {startDir}");
-        Console.WriteLine($"Directory: {directory}");
+        Console.WriteLine($"Directory: {string.Join(",", directories)}");
         Console.WriteLine($"Force: {force}");
         Console.WriteLine($"Recursive: {recursive}");
     }
 
-    private static string[]? __GetMatchingFolderPathes(string startDir, string[] directory, bool recursive)
+    private static string[]? __GetMatchingFolderPathes(string startDir, string[] directories, bool recursive)
     {
         List<string> Result = new List<string>();
         try
@@ -88,6 +91,7 @@ public static class Program
             Environment.Exit(1);
             return null;
         }
+        return Result.ToArray();
     }
 
     private static void __ValidateStartDirectory(string value)
